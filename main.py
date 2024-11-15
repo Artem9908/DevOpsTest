04f81c5dc771d5952fffd201118e5ffb226d483d
Artem Diachuk <mahmoud@zalt.me>
2024-11-15 19:10:15 +0000

    done

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import itertools
 
 digits = ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0']
-operators = 
+operators = ['+', '-', '']  
+
+for ops in itertools.product(operators, repeat=9):
+    expression = ''.join(d + o for d, o in zip(digits, ops)) + digits[-1]
+    try:
+        if eval(expression) == 200:
+            print(f"{expression} = 200")
+    except Exception:
+        continue
